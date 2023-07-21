553ed6de4d26afb272654818cb63609ddad09d17
Ahindra D <official.ahindra@gmail.com>
2023-07-22 03:49:28 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,94 @@
+require("dotenv").config();
+const { Socket } = require("socket.io");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const jwt = require("jsonwebtoken");
+const rfs = require("rotating-file-stream");
+
+
+// Connecting to MongoDB
+const DB_URL = process.env.DB_URL;
+const DB_Options = {
+    useNewURLParser: true,
+    useUnifiedTopology: true
+};
+mongoose.connect(DB_URL, DB_Options)
+    .then(() => console.log("Connected to HiveChat-MongoDB"))
+    .catch((err) => console.log("ERROR: ", err))
+
+
+// All Routes imported
+const authRouter = require("./Routes/Auth-Route");
+const userRouter = require("./Routes/User-Route");
+const groupRouter = require("./Routes/Group-Route");
+const postRouter = require("./Routes/Post-Route");
+const directMessagesRouter = require("./Routes/DirectMessages-Route");
+
+const app = express();
+
+//MIddleWares
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({}));
+
+// Logger to show logs in console 
+app.use(morgan("dev"));
+// add log stream to morgan to save logs in file
+const stream = rfs.createStream("file.log", {
+    size: "10M", // rotate every 10 MegaBytes written
+    interval: "1d", // rotate daily
+    compress: "gzip" // compress rotated files
+});
+app.use(morgan("dev", { stream }));
+
+
+//Routes related USAGE
+app.use("/auth", authRouter);
+//custom Auth middleware for data protection
+app.use("/user", authenticateRequest, userRouter);
+app.use("/posts", authenticateRequest, postRouter);
+app.use("/groups", authenticateRequest, groupRouter);
+app.use("/messages", authenticateRequest, directMessagesRouter);
+
+const httpServer = app.listen(process.env.PORT || 8000, () => {
+    const port = httpServer.address().port;
+    console.log(`Server running on ${port}`);
+});
+
+
+//AUTHORIZATION: custom middleware to prevent un-authorized access
+function authenticateRequest(request, response, next) {
+    const authHeaderInfo = request.headers["authorization"];
+    if (authHeaderInfo == undefined) {
+        return response.status(401).send("No token is provided!");
+    }
+    const token = authHeaderInfo.split(" ")[1];//Bearer <token>
+    if (token == undefined) {
+        return response.status(401).send("Proper token is NOT provided!");
+    }
+    try {
+        const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        request.userInfo = payload;
+        //console.log(request.userInfo);
+        next();
+    } catch (error) {
+        return response.status(401).send("Invalid token provided! ERROR: " + error.message);
+    }
+}
+
+
+//SOCKET_IO
+// const io = new Socket(httpServer);
+// let allRooms = {};
+// io.on("connection", (socket) => {
+//     console.log("Client Connected: ", socket.id);
+//     socket.emit("ID", socket.id);
+
+
+//     socket.on("disconnect", () => {
+//         console.log(`Client Disconnected: `, socket.id);
+//     });
+// })
